9eb7e5bc64344eb557030ecdfb289e0346cabf0b
Abhishek140298 <abhishekyadavfeb1498@gmail.com>
2026-08-07 21:15:28 +0530

    Timer done for otp

--- src/components/OTPComponent.jsx
+++ src/components/OTPComponent.jsx
@@ -1,23 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 //!Requirements
 //1)input boxex with border or underline
 //2)subit on submit button or max limit of otp words
 //3)heading
 //40timer ,duration
-
+//Make seprate component for timer
 function OTPComponent({
   number = 4,
   heading = "Enter the OTP",
   isSubmitButton = true,
-  timer = true,
-  duration = 5000,
+  //timer = true,
+  duration =120,
   onComplete,
 }) {
   const [otp, setOtp] = useState(new Array(number).fill(""));
   const inputRef = useRef([]);
+  const [countdown,setCountDown]=useState(duration)
+  const timerRef=useRef()
 
   const handleChange = (e, index) => {
-    console.log("index", otp);
     const value = e.target.value;
     let newOtp = [...otp];
     newOtp[index] = value;
@@ -25,58 +26,84 @@ function OTPComponent({
     setOtp(newOtp);
 
     if (value && index < number - 1) inputRef.current[index + 1].focus();
-    if(newOtp.every(digit=>digit!=""))console.log("Opt",newOtp.join(""))
-        //Call the function onComplete here if no submitbutton
+    if (newOtp.every((digit) => digit != ""))
+      console.log("Opt", newOtp.join(""));
+    //Call the function onComplete here if no submitbutton
   };
 
-  const handleKeyDown=(e,index)=>{
-    console.log("vjbjm",e.key)
-    switch(e.key){
-        case "Backspace":
-            if(otp[index]==""&&index>0){
-                inputRef.current[index-1].focus()
-            }
-            const newOptp=[...otp]
-            newOptp[index]=""
-            setOtp(newOptp)
-            break
-        case "ArrowLeft":
-            if(index>0){
-                inputRef.current[index-1].focus()
-            }    
-            break
-         case "ArrowRight":
-               if(index<otp.length){
-                inputRef.current[index+1].focus()
-            }    
-            break
+
+  const startTimer=()=>{
+     timerRef.current=setInterval(()=>{
+
+      //!using stale state of cuntdown
+     //if(countdown>0) setCountDown(prev=>prev-1)
+     setCountDown(prev=>{if(prev>0){
+      return prev-1
+     }})
+    },1000)
+    
+  }
+
+  const stopTimer=()=>{
+
+    clearInterval(timerRef.current)
+  }
+
+  useEffect(()=>{
+  startTimer()
+     return stopTimer
+
+  },[])
+
+  const handleKeyDown = (e, index) => {
+    switch (e.key) {
+      case "Backspace":
+        if (otp[index] == "" && index > 0) {
+          inputRef.current[index - 1].focus();
+        }
+        const newOptp = [...otp];
+        newOptp[index] = "";
+        setOtp(newOptp);
+        break;
+      case "ArrowLeft":
+        if (index > 0) {
+          inputRef.current[index - 1].focus();
+        }
+        break;
+      case "ArrowRight":
+        if (index < otp.length) {
+          inputRef.current[index + 1].focus();
+        }
+        break;
     }
-  }
-  
+  };
 
-  const handlePaste=(e)=>{
-    e.preventDefault()
-const pasted=e.clipboardData.getData("text").replace(/\D/g,"").slice(0,number).split("")
-console.log(
-    "Pasted",pasted
-)
-const newotp=[...otp]
-pasted.forEach((element,index) => {
-    console.log("skj",element,index)
-    newotp[index]=element
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pasted = e.clipboardData
+      .getData("text")
+      .replace(/\D/g, "")
+      .slice(0, number)
+      .split("");
+    console.log("Pasted", pasted);
+    const newotp = [...otp];
+    pasted.forEach((element, index) => {
+      console.log("skj", element, index);
+      newotp[index] = element;
+    });
 
-});
+    setOtp(newotp);
+    const focusedIndex = Math.min(pasted.length, number - 1);
 
-setOtp(newotp)
-const focusedIndex=Math.min(pasted.length,number-1)
-console.log("Newoptp",focusedIndex)
+    inputRef.current[focusedIndex]?.focus();
+  };
 
-inputRef.current[focusedIndex]?.focus()
-  }
+
+
+promise.then((value)=>console.log("Value",value))
 
   return (
     <div>
-      {  console.log("fchgvhjbjknlk",otp)}
       {otp.map((digit, index) => (
         <input
           key={index}
@@ -84,10 +111,11 @@ inputRef.current[focusedIndex]?.focus()
           maxLength={1}
           onChange={(e) => handleChange(e, index)}
           ref={(el) => (inputRef.current[index] = el)}
-          onKeyDown={(e)=>handleKeyDown(e,index)}
+          onKeyDown={(e) => handleKeyDown(e, index)}
           onPaste={handlePaste}
         />
       ))}
+      {timerRef.current==0?<div>Resend</div>:<div>{Math.floor(countdown/60)}:{countdown%60}</div>}
 
       <button>Submit</button>
     </div>
