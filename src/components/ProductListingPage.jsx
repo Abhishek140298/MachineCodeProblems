d05ae027b71bd07c47d88041affa089994fbc794
Abhishek140298 <abhishekyadavfeb1498@gmail.com>
2026-08-21 08:24:00 +0530

    Product listing and shoping cart

---
+++ src/components/ProductListingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+//!Requirements
+/*
+*Product listing with the categori wise ,
+*Use Infinite scroll and virtulization
+*Normalized state
+ */
+const ProductListingPage=()=>{
+    return <div>Rroduct</div>
+}
